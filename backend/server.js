f15f058fc896d514479cc1d3c7b797d86da17526
Vinayak Vaishnav <vinayakvaishnav25@gmail.com>
2026-03-13 01:36:21 +0530

    remove wildcard route

--- backend/server.js
+++ backend/server.js
@@ -22,9 +22,7 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(frontendPath, "index.html"));
 });
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(frontendPath, "index.html"));
-});
+
 
 /* ---------------- UPLOAD FOLDER ---------------- */
 
